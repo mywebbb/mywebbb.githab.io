1496a8133ec26f1150e989799ad828b47a57a40d
mywebbb <napishiii@bk.ru>
2021-11-20 20:00:29 +0300

    11

---
+++ matrix/src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    // Slow scroll
+    $('a[href^="#"').on('click', function() {
+
+        let href = $(this).attr('href');
+    
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        });
+        return false;
+    });
+
+});
+
+
+
+
