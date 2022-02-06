4a78ab6f39a8b905f678ebbfc450c0d8962c796c
mywebbb <napishiii@bk.ru>
2022-02-06 16:50:06 +0300

    1

--- oxygen/src/js/script.js
+++ oxygen/src/js/script.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
+    $('.slider__items').slick({
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-right.svg" alt=""></button>',
+      speed: 1200,
+      dots: true,
+      autoplay: true,
+
+      
+  
+    });
+
     $('ul.menu__list').on('click', 'li:not(.menu__list-item_active)', function() {
         $(this)
           .addClass('menu__list-item_active').siblings().removeClass('menu__list-item_active');
           
       
     });
-
-
-
 
     $('ul.portfolio__tabs').on('click', 'li:not(.portfolio__tab_active)', function() {
         $(this)
