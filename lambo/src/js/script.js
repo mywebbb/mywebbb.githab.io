d4ae861a8260c812653665b07a64a3b6ccc6bd7a
vadim <artemyqqq@yandex.ru>
2023-05-07 17:27:02 +0300

    1

--- lambo/src/js/script.js
+++ lambo/src/js/script.js
@@ -1,18 +1,30 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuMobile = document.querySelector('.menu-mobile');
 
+menuBtn.addEventListener('click', () => {
+    menuMobile.classList.toggle('menu--open');
+});
+
 
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     // direction: 'vertical',
-    slidesPerView: 2,
-    spaceBetween: 20,
+    
     loop: true,
     navigation: {
       nextEl: '.swiper-button-left',
       prevEl: '.swiper-button-right',
     },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        }
+    }
 
 });
 
